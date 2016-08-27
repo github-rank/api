3aa1d3d3426f6181b88e4cd084ba23c9564eae70
Ilya Kashlakov <i.kashlakov@yandex.ru>
2016-08-27 17:56:38 +0300

    Github API WIP

--- data-providers/github.js
+++ data-providers/github.js
@@ -1,15 +1,56 @@
 const GithubApi = require('github');
 const config = require('config');
 
+const githubConfig = config.get('providers.github');
+
 const githubProvider = new GithubApi({
-    debug: config.get('providers.github.debug'),
-    protocol: config.get('providers.github.protocol'),
-    host: config.get('providers.github.host'),
-    pathPrefix: config.get('providers.github.path-prefix'),
-    headers: config.get('providers.github.headers'),
-    followRedirects: config.get('providers.github.follow-redirects'),
-    timeout: config.get('providers.github.timeout'),
-    promise: require('bluebird')
+    debug: githubConfig.debug,
+    protocol: githubConfig.protocol,
+    host: githubConfig.host,
+    pathPrefix: githubConfig.pathPrefix,
+    headers: githubConfig.headers,
+    followRedirects: githubConfig.followRedirects,
+    timeout: githubConfig.timeout,
+    promise: require('vow')
 });
 
-module.exports = githubProvider;
+module.exports = {
+
+    /**
+     * Возвращает пользователя GitHub
+     *
+     * @param {String} username имя пользователя
+     * @returns {Promise}
+     */
+    getUser(username) {
+        return githubProvider.users.getForUser({
+            user: username
+        });
+    },
+
+    /**
+     * Возвращает публичные репозитории пользователя на GitHub
+     * 
+     * @param {String} username имя пользователя
+     * @returns {Promise}
+     */
+    getUserRepos(username) {
+        return githubProvider.repos.getForUser({
+            user: username
+        });
+    },
+
+    /**
+     * Возвращает контрибутеров репозитория
+     * 
+     * @param {String} username имя пользователя
+     * @param {String} repo имя репозитория
+     * @returns {Promise}
+     */
+    getRepoContributes(username, repo) {
+        return githubProvider.repos.getContributors({
+            user: username,
+            repo: repo
+        });
+    }
+};
